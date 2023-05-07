08c2f32c05fefca0a9b170dc06beef7a5d500094
Khem Sok <khemsok97@gmail.com>
2023-05-07 19:49:19 -0400

    moved assets

--- src/App.jsx
+++ src/App.jsx
@@ -3,19 +3,19 @@ function App() {
     <div className="flex h-screen overflow-hidden">
       <div className="m-auto">
         <div>
-          <img src="/logo.png" alt="Mastering React Testing" className="mx-auto w-80 h-80" />
+          <img src="/assets/img/logo.png" alt="Mastering React Testing" className="mx-auto w-80 h-80" />
           <h1 className="font-extrabold p-3 text-6xl text-center bg-gradient-to-r from-green-400 to-blue-500 text-transparent bg-clip-text">Mastering React Testing</h1>
           <p className="text-center text-sm text-gray-600 italic">Level up your React skills with comprehensive testing techniques - The ultimate guide to mastering React testing!</p>
           <div className="flex justify-center mt-5 space-x-5">
             <a href="https://github.com/khem-academy/mastering-react-testing" target="_blank" rel="noopener noreferrer" className="hover:text-blue-600 transition-colors duration-300">
               <div className="flex items-center space-x-2">
-                <img src="/github.svg" alt="GitHub" className="w-8 h-8" />
+                <img src="/assets/img/github.svg" alt="GitHub" className="w-8 h-8" />
                 <span>GitHub Repository</span>
               </div>
             </a>
             <a href="https://khemsok97.gumroad.com/l/kyhpsj" target="_blank" rel="noopener noreferrer" className="hover:text-green-600 transition-colors duration-300">
               <div className="flex items-center space-x-2">
-                <img src="/gumroad.png" alt="Gumroad" className="w-8 h-8" />
+                <img src="/assets/img/gumroad.png" alt="Gumroad" className="w-8 h-8" />
                 <span>Purchase on Gumroad</span>
               </div>
             </a>
